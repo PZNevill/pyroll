d6639989b7fef87de26141941bcec6884b051874
Philip Nevill <76637229+PZNevill@users.noreply.github.com>
2021-11-19 21:33:47 -0500

    Initial commit setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+setup(
+  name='pyroll',
+  description='cli for simulating the rolling of dice',
+  author='Philip Z Nevill',
+  author_email='pznevill.dev@gmail.com',
+  version='0.1.0',
+  packages=find_packages(include=['pyroll', 'pyroll.*']),
+)
